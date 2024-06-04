3f4172f19197fcf75cae1778bce6500ce5e07f93
Den <den.shpuganich@gmail.com>
2024-06-05 01:23:48 +0200

    React init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Tabs from './components/Tabs/Tabs';
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Tabs />
+      </header>
+
+        <h1>Hello, this is my first app!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tabs/Tabs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Tabs.scss';
+
+const Tabs = () => {
+    return (
+        <div className="Tabs">
+            <div className="Tab">1</div>
+            <div className="Tab">2</div>
+            <div className="Tab">3</div>
+        </div>
+    );
+};
+
+export default Tabs;
